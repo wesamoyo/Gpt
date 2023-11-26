48a36ddfd3686a20029b3fbd06f68831cfbd8f4a
wesamoyo <152051064+wesamoyo@users.noreply.github.com>
2023-11-26 20:31:39 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,62 +1,59 @@
 import streamlit as st
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import time
+from datetime import datetime
+import pyjokes
+from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
 
-# Load pre-trained GPT-2 model and tokenizer from raw GitHub URL
-model_url = 'https://github.com/wesamoyo/Gpt/raw/main/tf_model.h5'
-tokenizer = GPT2Tokenizer.from_pretrained(model_url)
-model = GPT2LMHeadModel.from_pretrained(model_url)
+# Load pre-trained GPT-2 model and tokenizer from raw URL
+model_url = "https://raw.githubusercontent.com/wesamoyo/Gpt/main/tf_model.h5"
+
+# Load the TensorFlow model directly
+model = TFGPT2LMHeadModel.from_pretrained(model_url)
+
+# Load the GPT-2 tokenizer (use the default GPT-2 tokenizer)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 # Set page configuration with title and icon
 st.set_page_config(
     page_title="HoundAI",
     page_icon="e9f83341-9417-4910-887f-e865c6d3e876.jpeg",
     layout="centered",
-    initial_sidebar_state="collapsed"  # Optional: Start with the sidebar collapsed
+    initial_sidebar_state="collapsed"
 )
 
-# Hide the Streamlit logo, main menu, and footer
-st.markdown("""
-    <style>
-        #MainMenu {visibility: hidden;}
-        footer {display: none;}
-        .viewerBadge_container__1QSob {display: none !important;}
-    </style>
-""", unsafe_allow_html=True)
+# ... (rest of your existing Streamlit app code)
 
-# Initialize "chats" in st.session_state if it doesn't exist
-if "chats" not in st.session_state:
-    st.session_state["chats"] = [{"messages": []}]
+# Inside the if statement where you process user input and generate responses:
+if prompt := st.text_input("User Input"):
+    response = ""
 
-# Display messages from the selected chat page
-chat = st.session_state["chats"][0]
-for msg in chat["messages"]:
-    if msg["role"] == "assistant":
-        st.chat_message("Hound").write(msg["content"])
+    # Check for greetings
+    if any(word in prompt.lower() for word in ["hello", "hi", "what's up", "whats up", "how are you", "how's it going"]):
+        response = "Hello! I'm here to assist you. How can I help you today?"
+    elif "who created you" in prompt.lower() or "who made you" in prompt.lower():
+        response = "I was created by Louis Wesamoyo. And he told me to tell you I am still under development."
+    elif "current time" in prompt.lower() or "time now" in prompt.lower():
+        current_time = datetime.now().strftime("%H:%M:%S")
+        response = f"The current time is {current_time}."
+    elif "tell me a joke" in prompt.lower() or "joke" in prompt.lower():
+        joke = pyjokes.get_joke()
+        response = f"Sure, here's a joke for you: {joke}."
+    elif "what's your name" in prompt.lower() or "what is your name" in prompt.lower():
+        assname = "HoundAi"
+        response = f"I'm called {assname}, your research-based assistant. Try me for any research, I've got you!"
+        # Display immediate result
+        st.text_area("Model Response", response)
+    elif "weather" in prompt.lower():
+        # Your weather code here (replace with GPT-2 compatible code)
+        pass
+    elif "news" in prompt.lower():
+        # Your news code here (replace with GPT-2 compatible code)
+        pass
     else:
-        st.chat_message(msg["role"]).write(msg["content"])
-
-# Add "New Chat" button to the sidebar
-new_chat_button = st.sidebar.button("New Chat")
-
-# Clear previous chats if the button is clicked
-if new_chat_button:
-    st.session_state["chats"][-1]["messages"] = []
-    # Set the button state to False to avoid immediate clearing on subsequent interactions
-    st.session_state["new_chat_button"] = True
-
-# Store the state of the new chat button
-st.session_state["new_chat_button"] = new_chat_button
-
-if prompt := st.chat_input():
-    st.session_state["chats"][-1]["messages"].append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    # Generate response using the GPT-2 model
-    input_ids = tokenizer.encode(prompt, return_tensors="pt")
-    output = model.generate(input_ids, max_length=150, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
-    generated_response = tokenizer.decode(output[0], skip_special_tokens=True)
+        # Use GPT-2 model to generate a response
+        input_ids = tokenizer.encode(prompt, return_tensors="tf")  # Use TensorFlow encoding
+        output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+        response = tokenizer.decode(output[0], skip_special_tokens=True)
 
     # Display the generated response
-    st.chat_message("Hound").write(generated_response)
-
-st.session_state["new_chat_button"] = new_chat_button
+    st.text_area("Model Response", response)
