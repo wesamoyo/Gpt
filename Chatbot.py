90ee386199c2608988ca415965755d741a839bec
wesamoyo <152051064+wesamoyo@users.noreply.github.com>
2023-11-27 07:28:03 +0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,32 +2,22 @@ import streamlit as st
 import time
 from datetime import datetime
 import pyjokes
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
+import requests
+from tensorflow.keras.models import load_model  # Assuming you used TensorFlow for your model
 
-# Load pre-trained GPT-2 model and tokenizer from raw URL
-model_url = "https://raw.githubusercontent.com/wesamoyo/Gpt/main/tf_model.h5"
+# Load your pre-trained model
+tf_model = load_model("tf_model.h5")
 
-# Load the TensorFlow model directly
-model = TFGPT2LMHeadModel.from_pretrained(model_url)
+# ... (the rest of your existing code)
 
-# Load the GPT-2 tokenizer (use the default GPT-2 tokenizer)
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# Replace the Wikipedia-related section with model-based response generation
+if prompt := st.chat_input():
+    st.session_state["chats"][-1]["messages"].append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-# Set page configuration with title and icon
-st.set_page_config(
-    page_title="HoundAI",
-    page_icon="e9f83341-9417-4910-887f-e865c6d3e876.jpeg",
-    layout="centered",
-    initial_sidebar_state="collapsed"
-)
-
-# ... (rest of your existing Streamlit app code)
-
-# Inside the if statement where you process user input and generate responses:
-if prompt := st.text_input("User Input"):
     response = ""
 
-    # Check for greetings
+    # Check for greetings (You can extend this logic for other intents)
     if any(word in prompt.lower() for word in ["hello", "hi", "what's up", "whats up", "how are you", "how's it going"]):
         response = "Hello! I'm here to assist you. How can I help you today?"
     elif "who created you" in prompt.lower() or "who made you" in prompt.lower():
@@ -35,25 +25,12 @@ if prompt := st.text_input("User Input"):
     elif "current time" in prompt.lower() or "time now" in prompt.lower():
         current_time = datetime.now().strftime("%H:%M:%S")
         response = f"The current time is {current_time}."
-    elif "tell me a joke" in prompt.lower() or "joke" in prompt.lower():
-        joke = pyjokes.get_joke()
-        response = f"Sure, here's a joke for you: {joke}."
-    elif "what's your name" in prompt.lower() or "what is your name" in prompt.lower():
-        assname = "HoundAi"
-        response = f"I'm called {assname}, your research-based assistant. Try me for any research, I've got you!"
-        # Display immediate result
-        st.text_area("Model Response", response)
-    elif "weather" in prompt.lower():
-        # Your weather code here (replace with GPT-2 compatible code)
-        pass
-    elif "news" in prompt.lower():
-        # Your news code here (replace with GPT-2 compatible code)
-        pass
+    # Add more conditions for other intents as needed
     else:
-        # Use GPT-2 model to generate a response
-        input_ids = tokenizer.encode(prompt, return_tensors="tf")  # Use TensorFlow encoding
-        output = model.generate(input_ids, max_length=100, num_return_sequences=1)
-        response = tokenizer.decode(output[0], skip_special_tokens=True)
+        # Use your pre-trained model to generate a response based on the user's input
+        # Replace the following line with the actual code for generating responses from your model
+        model_response = generate_response_with_model(tf_model, prompt)
+        response = model_response
 
     # Display the generated response
-    st.text_area("Model Response", response)
+    st.chat_message("Hound").write(response)
